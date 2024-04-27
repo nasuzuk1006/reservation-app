0da010886a853b07b34a5e65c0c77203ee4cad88
nasuzuk1006 <cbb28678@gmail.com>
2024-04-27 22:44:00 +0900

    フロントエンドとバックエンドの結合

--- src/app/product/product-detail/product-detail.component.ts
+++ src/app/product/product-detail/product-detail.component.ts
@@ -1,23 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductListComponent } from '../product-listings/product-listings.component';
 import { ActivatedRoute } from '@angular/router';
-import { products } from '../../products';
+import { ProductService } from '../shared/product.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [ProductListComponent],
+  imports: [ProductListComponent, CommonModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.scss'
 })
 export class ProductDetailComponent implements OnInit {
   product: any | undefined;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
   
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
-      this.product = products[+params.get('productId')!]
+      // this.product = this.productService.getProductById(params.get('productId')!)
+      const productObservable = this.productService.getProductById(params.get('productId')!)
+      productObservable.subscribe({
+        next: (data) => { this.product = data },
+        error: (err) => { console.error('次のエラーが発生しました：' + err) }
+      })
     })
   }
 }
