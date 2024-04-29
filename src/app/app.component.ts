1ec56c5bbbcd4ba969a3f818a27c69dd42fcd750
nasuzuk1006 <cbb28678@gmail.com>
2024-04-29 22:41:06 +0900

    デザイン作り込み

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ElementRef, Inject, InjectionToken, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
 import { NavbarComponent } from './common/navbar/navbar.component';
 import { HttpClientModule } from '@angular/common/http';
+import { Location, DOCUMENT } from '@angular/common';
+import { filter } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +13,62 @@ import { HttpClientModule } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'reservation-app';
+
+export class AppComponent implements OnInit {
+  private _router!: Subscription;
+  @ViewChild(NavbarComponent) navbar!: NavbarComponent;
+ 
+  constructor( 
+    private renderer : Renderer2, 
+    private router: Router, 
+    @Inject(DOCUMENT) private document: any, 
+    private element : ElementRef, 
+    public location: Location
+  ) {}
+  ngOnInit() {
+      var navbar : HTMLElement = this.element.nativeElement.children[0].children[0];
+      this._router = this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(() => {
+        if (window.outerWidth > 991) {
+            window.document.children[0].scrollTop = 0;
+        }else{
+            window.document.activeElement!.scrollTop = 0;
+        }
+        this.navbar.sidebarClose();
+      });
+      this.renderer.listen('window', 'scroll', (event) => {
+        const number = window.scrollY;
+        if (number > 150 || window.scrollY > 150) {
+          // add logic
+          navbar.classList.remove('navbar-transparent');
+        } else {
+          // remove logic
+          navbar.classList.add('navbar-transparent');
+        }
+      });
+      if (typeof window !== "undefined") {
+        var ua = window.navigator.userAgent;
+        var trident = ua.indexOf('Trident/');
+        if (trident > 0) {
+            // IE 11 => return version number
+            var rv = ua.indexOf('rv:');
+            var version = parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+        }
+      }
+      if (version!) {
+          var body = document.getElementsByTagName('body')[0];
+          body.classList.add('ie-background');
+ 
+      }
+ 
+  }
+  removeFooter() {
+      var titlee = this.location.prepareExternalUrl(this.location.path());
+      titlee = titlee.slice( 1 );
+      if(titlee === 'signup' || titlee === 'nucleoicons'){
+          return false;
+      }
+      else {
+          return true;
+      }
+  }
 }
